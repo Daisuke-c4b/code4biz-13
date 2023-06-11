c2c0d3e7e2786b2cd0e8cdb591a08a4887f3b083
Daisuke <mondo.piovoso@gmail.com>
2023-06-11 22:56:58 +0900

    課題3：基本ステータス（age、hunger、happiness、weightなど）の取り得る数値範囲の設定と判定

--- Tamagotchi.py
+++ Tamagotchi.py
@@ -14,32 +14,65 @@ class Tamagotchi:
         print(f"{self.name}にご飯を与えました")
         print("もぐもぐ!")
         self.age += 0
-        self.hunger += 5
-        self.happiness += 5
+        self.hunger = min(100, self.hunger + 5)
+        self.happiness = min(100, self.happiness + 5)
         self.weight += 2
+        self.check_game_over()
+        self.check_growth()
 
     def sleep(self):
         print(f"{self.name}は眠りにつきました")
         print("ぐーぐー…")
         self.age += 0
-        self.hunger -= 1
-        self.happiness += 1
+        self.hunger = max(0, self.hunger - 1)
+        self.happiness = min(100, self.happiness + 1)
         self.weight += 0
+        self.check_game_over()
+        self.check_growth()
 
     def play(self):
         print(f"{self.name}は遊びました")
         print("わいわい!!")
         self.age += 0
-        self.hunger -= 3
-        self.happiness += 1
-        self.weight -= 1
+        self.hunger = max(0, self.hunger - 3)
+        self.happiness = min(100, self.happiness + 1)
+        self.weight = max(0, self.weight - 1)
+        self.check_game_over()
+        self.check_growth()
 
     def update_time(self, hour):
         print(f"{hour}時間経過しました")
         self.age += 1 * hour
-        self.hunger -= 2 * hour
-        self.happiness -= 3 * hour
+        self.hunger = max(0, self.hunger - (2 * hour))
+        self.happiness = max(0, self.happiness - (3 * hour))
         self.weight += 1 * hour
+        self.check_game_over()
+        self.check_growth()
+
+    def check_game_over(self):
+        if self.hunger <= 0 or self.happiness <= 0:
+            print(f"{self.name}は餓死してしまいました。ゲームオーバー")
+            exit(0)
+
+    def check_growth(self):
+        current_name = self.name
+        new_name = self.name
+
+        if self.age >= 60 and self.weight >= 50:
+            new_name = "おやじっち"
+        elif self.age >= 18 and self.hunger >= 60 and self.happiness >= 60:
+            new_name = "まめっち"
+        elif self.age >= 4 and self.hunger >= 10 and self.happiness >= 10:
+            if self.hunger >= 30 and self.happiness >= 30:
+                new_name = "たまっち"
+            else:
+                new_name = "くちたっち"
+        elif self.age >= 2 and self.hunger >= 1 and self.happiness >= 1:
+            new_name = "まるっち"
+
+        if current_name != new_name:
+            self.name = new_name
+            print(f"{self.name}に進化しました")
 
     def __str__(self):
         self.status = textwrap.dedent(
@@ -106,17 +139,13 @@ def main():
 
         if choice == "1":
             tama.eat()
-            print(tama)
         elif choice == "2":
             tama.sleep()
-            print(tama)
         elif choice == "3":
             tama.play()
-            print(tama)
         elif choice == "4":
             hour = int(input("何時間経過させますか？（時間を数字で入力してください）： "))
             tama.update_time(hour)
-            print(tama)
         elif choice == "5":
             print(tama)
         elif choice == "6":
